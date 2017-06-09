455a07d9cead156a3c7d1b294f01f088c7e7816f
Mariano Giagante <mariano.giagante@gmail.com>
2017-06-09 03:39:48 -0300

    Implements solution for exercise simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+
+def echo(phrase)
+  phrase
+end
+
+def shout(phrase)
+  phrase.upcase
+end
+
+def repeat(phrase, times=2)
+  Array.new(times, phrase).join(' ')
+end
+
+def start_of_word(word, number_of_letters)
+  word[0..number_of_letters - 1]
+end
+
+def first_word(phrase)
+  phrase.split(' ').first
+end
+
+def titleize(phrase)
+  little = %w(and over the) 
+
+  words = phrase.split(' ')
+  words.first.capitalize!
+
+  words.map { |word| little.include?(word) ? word : word.capitalize }.join(' ')
+end
