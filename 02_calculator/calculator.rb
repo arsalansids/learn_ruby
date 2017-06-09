a12bd5c5aaccab1141effd00fdb94d2c6559c6ba
Mariano Giagante <mariano.giagante@gmail.com>
2017-06-09 01:32:44 -0300

    Implements solution to calculator exercise, including bonus tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,25 @@
 #write your code here
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  array.reduce(0, :+)
+end
+
+def multiply(*numbers)
+  numbers.reduce(:*)
+end
+
+def power(a, b)
+  Array.new(b, a).reduce(1, :*)
+end
+
+def factorial(number)
+  [0, 1].include?(number) ? 1 : number * factorial(number - 1)
+end
