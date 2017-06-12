345acdc6d082e5af25e57fa4b0e9dd2fd890b5fb
Mariano Giagante <mariano.giagante@gmail.com>
2017-06-12 00:53:14 -0300

    Implements solution for timer exercise; Uncomments #padded examples and places them before time_string examples for convenience when running the specs

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    self.seconds = 0
+  end
+
+  def time_string
+    hours = self.seconds / 3600
+    minutes = (self.seconds % 3600) / 60
+    seconds = (self.seconds % 3600) % 60
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
+  def padded(number)
+    number.to_s.size == 1 ? "0#{number}" : "#{number}"
+  end
 end
