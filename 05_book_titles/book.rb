2a67104ef5fdb851ae9dd9ccd124c134ee735863
Mariano Giagante <mariano.giagante@gmail.com>
2017-06-12 00:12:20 -0300

    Implements solution to the book titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
 # write your code here
+  DO_NOT_CAPITALIZE = %w( and in the of a an )
+
+  attr_reader :title
+
+  def title=(string)
+    words = string.split(' ')
+    capitalized = words.map do |word|
+      DO_NOT_CAPITALIZE.include?(word) ? word : word.capitalize
+    end
+    capitalized.first.capitalize!
+    @title = capitalized.join(' ')
+  end
 end
